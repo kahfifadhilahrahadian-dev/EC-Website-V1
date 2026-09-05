5043465b8ce71a565db8d2ac7d6a4be004cbc304
Kahfi <kahfifadhilahrahadian@gmail.com>
2026-09-05 19:45:53 +0700

    TABEL ANJG

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const currentDateText = document.getElementById('currentDateText');
 
     // PASTE YOUR GOOGLE APPS SCRIPT URL HERE IF AVAILABLE
-    const GOOGLE_SCRIPT_URL = 'PASTE_URL_GOOGLE_APPS_SCRIPT_DI_SINI';
+    const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby5gJf0NEpVUgp3IJWqsqZLf20AEB_8xFIoiIdbH49ZEefOymw1xmzGWP6IJmrJ4ZQRQA/exec';
 
     // Helper: Format Date to Indonesian Day and Date (e.g., Jumat, 12 September 2026)
     function getFormattedDate(dateObj) {
